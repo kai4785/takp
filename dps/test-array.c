7c66fce76a10e021e53f29fa1477c5a2c37488a4
Kai Meyer <kai@gnukai.com>
2019-03-23 09:59:57 -0600

    Adding a nested array test, and some helpers for code coverage

--- dps/test-array.c
+++ dps/test-array.c
@@ -1,57 +1,142 @@
 #include "array.h"
 #include "test.h"
 
-#define __test(_l, _r) \
-if((_l) != (_r)) { \
-    fprintf(stderr, "%s:%d: Comparison failed: (%lu == %lu)\n", __FILE__, __LINE__, (_l), (_r)); \
-    errors++; \
+int cmp_Signed(int64_t left, int64_t right, bool equals)
+{
+    return (equals != (left == right));
+}
+
+void print_Signed(int64_t value)
+{
+    fprintf(stderr, "(%"PRId64")", value);
+}
+
+int cmp_Unsigned(uint64_t left, uint64_t right, bool equals)
+{
+    return (equals != (left == right));
+}
+
+void print_Unsigned(uint64_t value)
+{
+    fprintf(stderr, "(%"PRIu64")", value);
+}
+
+#define test_int_eq(_x, _y) _test_eq(errors, cmp_Signed, print_Signed, _x, _y, true)
+#define test_int_neq(_x, _y) _test_eq(errors, cmp_Signed, print_Signed, _x, _y, true)
+
+#define test_uint_eq(_x, _y) _test_eq(errors, cmp_Unsigned, print_Signed, _x, _y, true)
+#define test_uint_neq(_x, _y) _test_eq(errors, cmp_Unsigned, print_Signed, _x, _y, true)
+
+void datum_dtor(void* datum)
+{
+    struct Array* array = (struct Array*)datum;
+    array->dtor(array);
+}
+
+int testNestedArrays()
+{
+    int errors = 0;
+    struct Array array;
+    Array_ctor(&array, sizeof(struct Array));
+    array.datum_dtor = datum_dtor;
+
+    /* Create an array of arrays like this:
+     * [
+     *  [ ],
+     *  [ 0 ],
+     *  [ 0, 1 ],
+     *  [ 0, 1, 2],
+     *  ...
+     */
+    for(int count = 0; count < 10; count++)
+    {
+        // Create and fill the nested array
+        struct Array nested;
+        Array_ctor(&nested, sizeof(int));
+        for(int value = 0; value < count; value++)
+        {
+            nested.push(&nested, &value);
+        }
+        // Push the nested array onto the array
+        array.push(&array, &nested);
+        // No nested.dtor
+    }
+    
+    // Check array[0]
+    {
+        struct Array* subZero = array.at(&array, 0);
+        test_uint_eq(subZero->size, 0);
+        test_uint_eq(subZero->capacity, 0);
+        test_uint_eq(subZero->datumSize, sizeof(int));
+    }
+    // Check array[2]
+    {
+        struct Array* subTwo = array.at(&array, 2);
+        test_uint_eq(subTwo->size, 2);
+        _test(errors, subTwo->capacity >= 2, true);
+        test_uint_eq(subTwo->datumSize, sizeof(int));
+        test_int_eq(*(int*)(subTwo->at(subTwo, 0)), 0);
+        test_int_eq(*(int*)(subTwo->at(subTwo, 1)), 1);
+    }
+    // Check array[9]
+    {
+        struct Array* subNine = array.at(&array, 9);
+        test_uint_eq(subNine->size, 9);
+        _test(errors, subNine->capacity >= 9, true);
+        test_uint_eq(subNine->datumSize, sizeof(int));
+        for(int value = 0; value < 9; value++)
+        {
+            test_int_eq(*(int*)(subNine->at(subNine, value)), value);
+        }
+    }
+
+    array.dtor(&array);
+    return errors;
 }
 
 int testArray()
 {
     int errors = 0;
+    struct Array array;
+    Array_ctor(&array, sizeof(int));
+
+    array.resize(&array, 10);
+    test_uint_eq(array.size, 0);
+    test_uint_eq(array.capacity, 10);
+    test_uint_eq(array.datumSize, sizeof(int));
+    int newValue = 0;
+    array.push(&array, &newValue);
+    newValue++;
+    array.push(&array, &newValue);
+    newValue++;
+    test_int_eq(array.size, 2);
+    test_int_eq(array.capacity, 10);
+    test_int_eq(*(int*)array.at(&array, 0), 0);
+    test_int_eq(*(int*)array.at(&array, 1), 1);
+    for(; newValue < 10; newValue++)
     {
-        struct Array array;
-        Array_ctor(&array, sizeof(unsigned long));
-
-        array.resize(&array, 10);
-        __test(array.size, 0UL);
-        __test(array.capacity, 10UL);
-        __test(array.datumSize, sizeof(unsigned long));
-        unsigned long newValue = 0;
         array.push(&array, &newValue);
-        newValue++;
-        array.push(&array, &newValue);
-        newValue++;
-        __test(array.size, 2UL);
-        __test(array.capacity, 10UL);
-        __test(*(unsigned long*)array.at(&array, 0), 0UL);
-        __test(*(unsigned long*)array.at(&array, 1), 1UL);
-        for(; newValue < 10; newValue++)
-        {
-            array.push(&array, &newValue);
-        }
-        __test(array.size, 10UL);
-        __test(array.capacity, 10UL);
-        __test(*(unsigned long*)array.at(&array, 8), 8UL);
-        __test(*(unsigned long*)array.at(&array, 9), 9UL);
-        array.push(&array, &newValue);
-        newValue++;
-        __test(array.size, 11UL);
-        __test(array.capacity, 20UL);
-        __test(*(unsigned long*)array.at(&array, 10), 10UL);
-        for(; newValue < 1024; newValue++)
-        {
-            array.push(&array, &newValue);
-        }
-        __test(array.size, 1024UL);
-        __test(array.capacity, 1024UL);
-
-        array.resize(&array, 10);
-        __test(array.size, 10UL);
-        __test(array.capacity, 10UL);
-        array.dtor(&array);
     }
+    test_uint_eq(array.size, 10);
+    test_uint_eq(array.capacity, 10);
+    test_int_eq(*(int*)array.at(&array, 8), 8);
+    test_int_eq(*(int*)array.at(&array, 9), 9);
+    array.push(&array, &newValue);
+    newValue++;
+    test_uint_eq(array.size, 11);
+    test_uint_eq(array.capacity, 20);
+    test_int_eq(*(int*)array.at(&array, 10), 10);
+    for(; newValue < 1024; newValue++)
+    {
+        array.push(&array, &newValue);
+    }
+    test_uint_eq(array.size, 1024);
+    test_uint_eq(array.capacity, 1024);
+
+    array.resize(&array, 10);
+    test_uint_eq(array.size, 10);
+    test_uint_eq(array.capacity, 10);
+    array.dtor(&array);
     return errors;
 }
 
@@ -59,5 +144,6 @@ int main()
 {
     int errors = 0;
     errors += testArray();
+    errors += testNestedArrays();
     return errors;
 }
